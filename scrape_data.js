8ea553395d8845ddcf8ca45f7f39d7a84d81460f
namtrhg <namtrhg@gmail.com>
2024-04-17 15:53:12 +0700

    fix: update chunckSize by 50

--- scrape_data.js
+++ scrape_data.js
@@ -169,7 +169,7 @@ const fs = require("fs").promises;
 			spinner.succeed(`Successfully scraped post ${index + 1}/${totalPages}: ${pageURL}`);
 		}
 		// Create promises for each set of 20 pages and wait for all of them to complete
-		const chunkSize = 100;
+		const chunkSize = 50;
 		for (let i = 0; i < totalPages; i += chunkSize) {
 			const promises = jsonData.ContentsList.slice(i, i + chunkSize).map((content, index) => processPage(content, i + index, totalPages));
 			await Promise.all(promises);
